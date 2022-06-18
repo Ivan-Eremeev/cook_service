600592a6d0a8ed76fa638cf2b1cc68e922dd07b2
Ivan <ivan.frontcoder@gmail.com>
2022-06-18 15:28:55 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
   
   // Swiper
   const sliderBanners = new Swiper('#sliderBanners', {
+    slidesPerView: 1.16,
     pagination: {
       el: '.slider__pagination',
     },
+    breakpoints: {
+      1200: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      }
+    }
   });
 
 });
